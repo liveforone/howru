5e6f0526dabf7a2092821123f7525796a94004f0
liveforone <yc4852@gmail.com>
2023-07-17 18:07:34 +0900

    Add member command service validator.

---
+++ src/main/kotlin/howru/howru/member/service/validator/MemberServiceValidator.kt
@@ -0,0 +1,19 @@
+package howru.howru.member.service.validator
+
+import howru.howru.exception.exception.MemberException
+import howru.howru.exception.message.MemberExceptionMessage
+import howru.howru.member.repository.MemberRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class MemberServiceValidator @Autowired constructor(
+    private val memberRepository: MemberRepository
+) {
+
+    fun validateDuplicateEmail(email: String) {
+        check(memberRepository.findIdByEmailNullableForValidate(email) == null) {
+            throw MemberException(MemberExceptionMessage.DUPLICATE_EMAIL)
+        }
+    }
+}
