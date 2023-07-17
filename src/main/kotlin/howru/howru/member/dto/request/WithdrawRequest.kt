5c2b645f955986613961fdcc62a740763ca2741e
liveforone <yc4852@gmail.com>
2023-07-17 18:06:04 +0900

    Add withdraw dto.

---
+++ src/main/kotlin/howru/howru/member/dto/request/WithdrawRequest.kt
@@ -0,0 +1,8 @@
+package howru.howru.member.dto.request
+
+import jakarta.validation.constraints.NotBlank
+
+data class WithdrawRequest(
+    @field:NotBlank(message = "비밀번호를 입력하세요.")
+    val pw: String?
+)
