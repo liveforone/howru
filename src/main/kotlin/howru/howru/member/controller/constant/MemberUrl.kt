729fdfb4206db6df4094666777baa1cb3412258c
liveforone <yc4852@gmail.com>
2023-07-17 18:06:45 +0900

    Add member url.

---
+++ src/main/kotlin/howru/howru/member/controller/constant/MemberUrl.kt
@@ -0,0 +1,11 @@
+package howru.howru.member.controller.constant
+
+object MemberUrl {
+    const val SIGNUP_MEMBER = "/member/signup"
+    const val LOGIN = "/member/login"
+    const val INFO = "/member/info"
+    const val UPDATE_EMAIL = "/member/{uuid}/update/email"
+    const val UPDATE_PASSWORD = "/member/{uuid}/update/password"
+    const val WITHDRAW = "/member/{uuid}/withdraw"
+    const val PROHIBITION = "/prohibition"
+}
