bc28d4c9bc51ac01617476b42c325418998d0564
liveforone <yc4852@gmail.com>
2023-07-17 18:05:51 +0900

    Add update email dto.

---
+++ src/main/kotlin/howru/howru/member/dto/update/UpdateEmail.kt
@@ -0,0 +1,8 @@
+package howru.howru.member.dto.update
+
+import jakarta.validation.constraints.NotBlank
+
+data class UpdateEmail(
+    @field:NotBlank(message = "변경할 이메일을 입력하세요.")
+    val newEmail: String?
+)
