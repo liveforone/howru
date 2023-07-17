797546ec490acd2e40bc2f70eb3f9b9fdee54496
liveforone <yc4852@gmail.com>
2023-07-17 18:03:51 +0900

    Add member constant.

---
+++ src/main/kotlin/howru/howru/member/domain/constant/MemberConstant.kt
@@ -0,0 +1,8 @@
+package howru.howru.member.domain.constant
+
+object MemberConstant {
+    const val ADMIN_EMAIL = "admin_howru@gmail.com"
+    const val BASIC_REPORT: Long = 0
+    const val BASIC_VARIATION: Long = 1
+    const val SUSPEND_LIMIT: Long = 5
+}
