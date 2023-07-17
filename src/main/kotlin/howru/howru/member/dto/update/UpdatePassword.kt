97a157e1fcecd2b3e2c824371b366551a67579b0
liveforone <yc4852@gmail.com>
2023-07-17 18:05:58 +0900

    Add update password dto.

---
+++ src/main/kotlin/howru/howru/member/dto/update/UpdatePassword.kt
@@ -0,0 +1,10 @@
+package howru.howru.member.dto.update
+
+import jakarta.validation.constraints.NotBlank
+
+data class UpdatePassword(
+    @field:NotBlank(message = "새 비밀번호를 입력하세요.")
+    val newPassword: String?,
+    @field:NotBlank(message = "기존 비밀번호를 입력하세요.")
+    val oldPassword: String?
+)
