f0f1aef83712e2ee050cba92bbdd8c93d0106fc5
liveforone <yc4852@gmail.com>
2023-07-18 19:33:24 +0900

    Add subscribe controller advice.

---
+++ src/main/kotlin/howru/howru/exception/controllerAdvice/SubscribeControllerAdvice.kt
@@ -0,0 +1,16 @@
+package howru.howru.exception.controllerAdvice
+
+import howru.howru.exception.exception.SubscribeException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class SubscribeControllerAdvice {
+    @ExceptionHandler(SubscribeException::class)
+    fun subscribeExceptionHandel(subscribeException: SubscribeException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(subscribeException.subscribeExceptionMessage.status)
+            .body(subscribeException.message)
+    }
+}
