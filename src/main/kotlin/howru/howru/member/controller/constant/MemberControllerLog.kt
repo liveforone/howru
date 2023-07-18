549568a00e2d6d7c6722c72d10eab170c16dc503
liveforone <yc4852@gmail.com>
2023-07-19 00:06:19 +0900

    Add member lock on and off controller log.

--- src/main/kotlin/howru/howru/member/controller/constant/MemberControllerLog.kt
+++ src/main/kotlin/howru/howru/member/controller/constant/MemberControllerLog.kt
@@ -5,5 +5,7 @@ enum class MemberControllerLog(val log: String) {
     LOGIN_SUCCESS("로그인 성공"),
     UPDATE_EMAIL_SUCCESS("이메일 변경 성공"),
     UPDATE_PW_SUCCESS("비밀번호 변경 성공"),
+    LOCK_ON_SUCCESS("회원 잠금 성공"),
+    LOCK_OFF_SUCCESS("회원 잠금 해제 성공"),
     WITHDRAW_SUCCESS("회원탈퇴 성공")
 }
