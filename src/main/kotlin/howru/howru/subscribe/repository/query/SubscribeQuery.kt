76b471c4326084081556d443ceeae70e6990a881
liveforone <yc4852@gmail.com>
2023-07-19 19:26:25 +0900

    Add is followee query.

--- src/main/kotlin/howru/howru/subscribe/repository/query/SubscribeQuery.kt
+++ src/main/kotlin/howru/howru/subscribe/repository/query/SubscribeQuery.kt
@@ -1,7 +1,12 @@
 package howru.howru.subscribe.repository.query
 
 object SubscribeQuery {
-    const val FOLLOW_EACH = "SELECT CASE WHEN EXISTS (" +
+    const val IS_FOLLOWEE = "SELECT CASE WHEN EXISTS (" +
+            "SELECT 1 FROM Subscribe s " +
+            "WHERE s.followeeUUID = :followeeUUID AND s.followerUUID = :followerUUID" +
+            ") THEN true ELSE false END"
+
+    const val IS_FOLLOW_EACH = "SELECT CASE WHEN EXISTS (" +
             "SELECT 1 FROM Subscribe s1 " +
             "WHERE s1.followeeUUID = :followeeUUID AND s1.followerUUID = :followerUUID" +
             ") AND EXISTS (" +
