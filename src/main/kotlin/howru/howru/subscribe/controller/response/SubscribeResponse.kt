5848ef56464cb688d4eed38550bd71e844089385
liveforone <yc4852@gmail.com>
2023-07-19 13:22:15 +0900

    Add subscribe response.

---
+++ src/main/kotlin/howru/howru/subscribe/controller/response/SubscribeResponse.kt
@@ -0,0 +1,17 @@
+package howru.howru.subscribe.controller.response
+
+import howru.howru.subscribe.dto.response.SubscribeInfo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+object SubscribeResponse {
+    private const val SUBSCRIBE_SUCCESS = "구독을 성공적으로 완료하였습니다."
+    private const val UNSUBSCRIBE_SUCCESS = "구독 취소를 성공적으로 완료하였습니다."
+
+    fun getFollowingSuccess(subscribes: List<SubscribeInfo>) = ResponseEntity.ok(subscribes)
+    fun getFollowerSuccess(subscribes: List<SubscribeInfo>) = ResponseEntity.ok(subscribes)
+    fun countFollowingSuccess(countOfFollowing: Long) = ResponseEntity.ok(countOfFollowing)
+    fun countFollowerSuccess(countOfFollower: Long) = ResponseEntity.ok(countOfFollower)
+    fun subscribeSuccess() = ResponseEntity.status(HttpStatus.CREATED).body(SUBSCRIBE_SUCCESS)
+    fun unsubscribeSuccess() = ResponseEntity.ok(UNSUBSCRIBE_SUCCESS)
+}
