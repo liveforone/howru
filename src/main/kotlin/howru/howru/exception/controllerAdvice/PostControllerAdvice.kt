6759d397585995b0dffd2857b12655d4e1117924
liveforone <yc4852@gmail.com>
2023-07-20 16:07:21 +0900

    Add post controller advice.

---
+++ src/main/kotlin/howru/howru/exception/controllerAdvice/PostControllerAdvice.kt
@@ -0,0 +1,16 @@
+package howru.howru.exception.controllerAdvice
+
+import howru.howru.exception.exception.PostException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class PostControllerAdvice {
+    @ExceptionHandler(PostException::class)
+    fun postExceptionHandle(postException: PostException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(postException.postExceptionMessage.status)
+            .body(postException.message)
+    }
+}
