6181198903c055e11d23899774afb7a37a1c57ea
liveforone <yc4852@gmail.com>
2023-07-20 16:08:25 +0900

    Add post repo constant.

---
+++ src/main/kotlin/howru/howru/post/repository/constant/PostRepoConstant.kt
@@ -0,0 +1,9 @@
+package howru.howru.post.repository.constant
+
+object PostRepoConstant {
+    const val LIMIT_PAGE = 10
+    const val RECOMMEND_LIMIT_PAGE = 3
+    const val START_OFFSET = 0
+    const val RANDOM_LIST_SIZE = 10
+    const val RANDOM_START_ID = 1
+}
