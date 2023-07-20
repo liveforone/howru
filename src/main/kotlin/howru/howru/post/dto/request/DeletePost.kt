cadb2412861086f1470c8bca9a2df06e30a99238
liveforone <yc4852@gmail.com>
2023-07-20 16:07:50 +0900

    Add delete post dto.

---
+++ src/main/kotlin/howru/howru/post/dto/request/DeletePost.kt
@@ -0,0 +1,11 @@
+package howru.howru.post.dto.request
+
+import jakarta.validation.constraints.NotNull
+import java.util.UUID
+
+data class DeletePost(
+    @field:NotNull(message = "게시글 외부식별자를 입력하세요.")
+    val uuid: UUID?,
+    @field:NotNull(message = "작성자의 외부식별자를 입력하세요.")
+    val writerUUID: UUID?
+)
