1aae0323ca7ad118f3a37e77c314d12f5d52fb3a
liveforone <yc4852@gmail.com>
2023-07-20 16:07:41 +0900

    Add update content dto.

---
+++ src/main/kotlin/howru/howru/post/dto/update/UpdateContent.kt
@@ -0,0 +1,16 @@
+package howru.howru.post.dto.update
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import java.util.UUID
+
+data class UpdateContent(
+    @field:NotNull(message = "게시글 외부식별자를 입력하세요.")
+    val uuid: UUID?,
+    @field:NotNull(message = "작성자의 외부식별자를 입력하세요.")
+    val writerUUID: UUID?,
+    @field:NotBlank(message = "게시글을 입력하세요.")
+    @field:Size(max = 800)
+    val content: String?
+)
