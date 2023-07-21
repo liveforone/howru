37c438fc5d6e5fc1d1d7783cebb3de597408eb94
liveforone <yc4852@gmail.com>
2023-07-21 13:49:13 +0900

    Add post response.

---
+++ src/main/kotlin/howru/howru/post/controller/response/PostResponse.kt
@@ -0,0 +1,22 @@
+package howru.howru.post.controller.response
+
+import howru.howru.post.dto.response.PostInfo
+import org.springframework.http.ResponseEntity
+
+object PostResponse {
+    private const val CREATE_POST_SUCCESS = "게시글을 성공적으로 등록하였습니다."
+    private const val EDIT_CONTENT_SUCCESS = "게시글을 성공적으로 수정하였습니다."
+    private const val DELETE_POST_SUCCESS = "게시글을 성공적으로 삭제하였습니다."
+
+    fun postDetailSuccess(postDetail: PostInfo) = ResponseEntity.ok(postDetail)
+    fun myPostSuccess(myPosts: List<PostInfo>) = ResponseEntity.ok(myPosts)
+    fun allPostSuccess(allPosts: List<PostInfo>) = ResponseEntity.ok(allPosts)
+    fun postOfWriterSuccess(postsOfWriter: List<PostInfo>) = ResponseEntity.ok(postsOfWriter)
+    fun postOfFolloweeSuccess(postsOfFollowee: List<PostInfo>) = ResponseEntity.ok(postsOfFollowee)
+    fun recommendPostSuccess(recommendPosts: List<PostInfo>) = ResponseEntity.ok(recommendPosts)
+    fun randomPostSuccess(randomPosts: List<PostInfo>) = ResponseEntity.ok(randomPosts)
+    fun countPostOfWriterSuccess(countPost: Long) = ResponseEntity.ok(countPost)
+    fun createPostSuccess() = ResponseEntity.ok(CREATE_POST_SUCCESS)
+    fun editPostSuccess() = ResponseEntity.ok(EDIT_CONTENT_SUCCESS)
+    fun deletePostSuccess() = ResponseEntity.ok(DELETE_POST_SUCCESS)
+}
