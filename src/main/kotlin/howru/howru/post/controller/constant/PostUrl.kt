0b7162224649070d1be5bbcc0b5aa4fb654d86c7
liveforone <yc4852@gmail.com>
2023-07-21 13:49:06 +0900

    Add post url.

---
+++ src/main/kotlin/howru/howru/post/controller/constant/PostUrl.kt
@@ -0,0 +1,15 @@
+package howru.howru.post.controller.constant
+
+object PostUrl {
+    const val DETAIL = "/post/detail/{uuid}"
+    const val MY_POST = "/post/my-post/{memberUUID}"
+    const val ALL_POST = "/post/all"
+    const val POST_OF_WRITER = "/post/writer/{writerUUID}"
+    const val POST_OF_FOLLOWEE = "/post/followee/{followerUUID}"
+    const val RECOMMEND = "/post/recommend"
+    const val RANDOM = "/post/random"
+    const val COUNT_POST_BY_WRITER = "/post/count/{writerUUID}"
+    const val CREATE = "/post/create"
+    const val EDIT_CONTENT = "/post/edit/content"
+    const val DELETE = "/post/delete"
+}
