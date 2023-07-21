63648fb7180e9a0fedaf5a9172483ec77b2277ba
liveforone <yc4852@gmail.com>
2023-07-21 13:48:59 +0900

    Add post param.

---
+++ src/main/kotlin/howru/howru/post/controller/constant/PostParam.kt
@@ -0,0 +1,10 @@
+package howru.howru.post.controller.constant
+
+object PostParam {
+    const val UUID = "uuid"
+    const val MEMBER_UUID = "memberUUID"
+    const val WRITER_UUID = "writerUUID"
+    const val FOLLOWER_UUID = "followerUUID"
+    const val LAST_UUID = "lastUUID"
+    const val CONTENT = "content"
+}
