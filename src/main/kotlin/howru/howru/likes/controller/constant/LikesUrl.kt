fc5113e4808ba9a763b654ecb23fc8fb8f8b6af4
liveforone <yc4852@gmail.com>
2023-07-23 17:37:13 +0900

    Add likes url.

---
+++ src/main/kotlin/howru/howru/likes/controller/constant/LikesUrl.kt
@@ -0,0 +1,8 @@
+package howru.howru.likes.controller.constant
+
+object LikesUrl {
+    const val LIKES_BELONG_MEMBER = "/likes/belong/member/{memberUUID}"
+    const val LIKES_BELONG_POST = "/likes/belong/post/{postUUID}"
+    const val LIKE = "/likes/like"
+    const val DISLIKE = "/likes/dislike"
+}
