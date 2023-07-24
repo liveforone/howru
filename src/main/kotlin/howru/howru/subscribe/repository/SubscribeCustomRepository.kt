d3471806cc8521a77a1689d6b71fda4b8b020988
liveforone <yc4852@gmail.com>
2023-07-24 16:22:07 +0900

    Add no offset paging method in composite table.

--- src/main/kotlin/howru/howru/subscribe/repository/SubscribeCustomRepository.kt
+++ src/main/kotlin/howru/howru/subscribe/repository/SubscribeCustomRepository.kt
@@ -7,8 +7,8 @@ import kotlin.collections.List
 
 interface SubscribeCustomRepository {
     fun findOneByUUID(followeeUUID: UUID, followerUUID: UUID): Subscribe
-    fun findSubscribesByFollower(followerUUID: UUID): List<SubscribeInfo>
-    fun findSubscribesByFollowee(followeeUUID: UUID): List<SubscribeInfo>
+    fun findSubscribesByFollower(followerUUID: UUID, lastFolloweeUUID: UUID?, lastFollowerUUID: UUID?): List<SubscribeInfo>
+    fun findSubscribesByFollowee(followeeUUID: UUID, lastFolloweeUUID: UUID?, lastFollowerUUID: UUID?): List<SubscribeInfo>
     fun findFollowee(followerUUID: UUID): List<UUID>
     fun countSubscribesByFollower(followerUUID: UUID): Long
     fun countFollowersByFollowee(followeeUUID: UUID): Long
--- src/main/kotlin/howru/howru/subscribe/repository/SubscribeRepositoryImpl.kt
+++ src/main/kotlin/howru/howru/subscribe/repository/SubscribeRepositoryImpl.kt
@@ -1,13 +1,16 @@
 package howru.howru.subscribe.repository
 
+import com.linecorp.kotlinjdsl.query.spec.predicate.PredicateSpec
 import com.linecorp.kotlinjdsl.querydsl.expression.col
 import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
 import com.linecorp.kotlinjdsl.spring.data.listQuery
+import com.linecorp.kotlinjdsl.spring.data.querydsl.SpringDataCriteriaQueryDsl
 import com.linecorp.kotlinjdsl.spring.data.singleQuery
 import howru.howru.exception.exception.SubscribeException
 import howru.howru.exception.message.SubscribeExceptionMessage
 import howru.howru.subscribe.domain.Subscribe
 import howru.howru.subscribe.dto.response.SubscribeInfo
+import howru.howru.subscribe.repository.constant.SubscribeRepoConstant
 import jakarta.persistence.NoResultException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
@@ -30,7 +33,7 @@ class SubscribeRepositoryImpl @Autowired constructor(
         }
     }
 
-    override fun findSubscribesByFollower(followerUUID: UUID): List<SubscribeInfo> {
+    override fun findSubscribesByFollower(followerUUID: UUID, lastFolloweeUUID: UUID?, lastFollowerUUID: UUID?): List<SubscribeInfo> {
         return queryFactory.listQuery {
             select(listOf(
                 col(Subscribe::followeeUUID),
@@ -38,11 +41,13 @@ class SubscribeRepositoryImpl @Autowired constructor(
             ))
             from(Subscribe::class)
             where(col(Subscribe::followerUUID).equal(followerUUID))
+            where(ltLastTimestamp(lastFolloweeUUID, lastFollowerUUID))
             orderBy(col(Subscribe::timestamp).desc())
+            limit(SubscribeRepoConstant.LIMIT_PAGE)
         }
     }
 
-    override fun findSubscribesByFollowee(followeeUUID: UUID): List<SubscribeInfo> {
+    override fun findSubscribesByFollowee(followeeUUID: UUID, lastFolloweeUUID: UUID?, lastFollowerUUID: UUID?): List<SubscribeInfo> {
         return queryFactory.listQuery {
             select(listOf(
                 col(Subscribe::followeeUUID),
@@ -50,7 +55,9 @@ class SubscribeRepositoryImpl @Autowired constructor(
             ))
             from(Subscribe::class)
             where(col(Subscribe::followeeUUID).equal(followeeUUID))
+            where(ltLastTimestamp(lastFolloweeUUID, lastFollowerUUID))
             orderBy(col(Subscribe::timestamp).desc())
+            limit(SubscribeRepoConstant.LIMIT_PAGE)
         }
     }
 
@@ -77,5 +84,22 @@ class SubscribeRepositoryImpl @Autowired constructor(
             from(Subscribe::class)
             where(col(Subscribe::followeeUUID).equal(followeeUUID))
         }
+    }
+
+    private fun findLastTimestamp(lastFolloweeUUID: UUID, lastFollowerUUID: UUID): Int {
+        return queryFactory.singleQuery {
+            select(listOf(col(Subscribe::timestamp)))
+            from(Subscribe::class)
+            where(col(Subscribe::followeeUUID).equal(lastFolloweeUUID).and(col(Subscribe::followerUUID).equal(lastFollowerUUID)))
+        }
+    }
+
+    private fun <T> SpringDataCriteriaQueryDsl<T>.ltLastTimestamp(lastFolloweeUUID: UUID?, lastFollowerUUID: UUID?): PredicateSpec? {
+        val findTimestamp = lastFolloweeUUID?.let { followee ->
+            lastFollowerUUID?.let { follower ->
+                findLastTimestamp(followee, follower)
+            }
+        }
+        return findTimestamp?.let { and(col(Subscribe::timestamp).lessThan(it)) }
     }
 }
