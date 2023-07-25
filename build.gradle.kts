7ab0b775ae29a4627f17d4a3a01778d4a8d388a0
liveforone <yc4852@gmail.com>
2023-07-25 16:02:32 +0900

    Add all open and upgrade spring boot version to 3.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.1"
-	id("io.spring.dependency-management") version "1.1.0"
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
 	kotlin("jvm") version "1.9.0"
 	kotlin("plugin.spring") version "1.9.0"
 	kotlin("plugin.jpa") version "1.9.0"
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
 }
 
 group = "howru"
