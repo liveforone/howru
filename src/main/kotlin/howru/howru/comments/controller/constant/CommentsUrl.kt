571fd46eb47fd956b1f3fff879cb01c146a0d5ac
liveforone <yc4852@gmail.com>
2023-07-25 16:06:34 +0900

    Add comments url.

---
+++ src/main/kotlin/howru/howru/comments/controller/constant/CommentsUrl.kt
@@ -0,0 +1,10 @@
+package howru.howru.comments.controller.constant
+
+object CommentsUrl {
+    const val DETAIL = "/comments/detail/{uuid}"
+    const val COMMENTS_BY_WRITER = "/comments/writer/{writerUUID}"
+    const val COMMENTS_BY_POST = "/comments/post/{postUUID}"
+    const val CREATE = "/comments/create"
+    const val EDIT = "/comments/edit"
+    const val DELETE = "/comments/delete"
+}
