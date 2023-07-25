a51463197e9996036c67e7caf1f17b866e3ab882
liveforone <yc4852@gmail.com>
2023-07-25 16:04:36 +0900

    Add delete comments dto.

---
+++ src/main/kotlin/howru/howru/comments/dto/request/DeleteComments.kt
@@ -0,0 +1,11 @@
+package howru.howru.comments.dto.request
+
+import jakarta.validation.constraints.NotNull
+import java.util.UUID
+
+data class DeleteComments(
+    @field:NotNull(message = "댓글의 외부식별자를 입력하세요.")
+    val uuid: UUID?,
+    @field:NotNull(message = "작성자의 외부식별자를 입력하세요.")
+    val writerUUID: UUID?
+)
