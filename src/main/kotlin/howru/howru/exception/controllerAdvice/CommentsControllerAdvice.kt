5f26b90bb40164cc4e998cad4ab8ac17b34d0c9e
liveforone <yc4852@gmail.com>
2023-07-25 16:03:59 +0900

    Add comments controller advice.

---
+++ src/main/kotlin/howru/howru/exception/controllerAdvice/CommentsControllerAdvice.kt
@@ -0,0 +1,16 @@
+package howru.howru.exception.controllerAdvice
+
+import howru.howru.exception.exception.CommentsException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class CommentsControllerAdvice {
+    @ExceptionHandler(CommentsException::class)
+    fun commentsExceptionHandle(commentsException: CommentsException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(commentsException.commentsExceptionMessage.status)
+            .body(commentsException.message)
+    }
+}
