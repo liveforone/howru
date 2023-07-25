3ebee816d8f4725c2c3941f998a8cf1bdb684e42
liveforone <yc4852@gmail.com>
2023-07-25 16:05:57 +0900

    Add comments command service.

---
+++ src/main/kotlin/howru/howru/comments/service/command/CommentsCommandService.kt
@@ -0,0 +1,45 @@
+package howru.howru.comments.service.command
+
+import howru.howru.comments.domain.Comments
+import howru.howru.comments.dto.request.CreateComments
+import howru.howru.comments.dto.request.DeleteComments
+import howru.howru.comments.dto.update.UpdateCommentsContent
+import howru.howru.comments.repository.CommentsRepository
+import howru.howru.member.repository.MemberRepository
+import howru.howru.post.repository.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+@Transactional
+class CommentsCommandService @Autowired constructor(
+    private val commentsRepository: CommentsRepository,
+    private val memberRepository: MemberRepository,
+    private val postRepository: PostRepository
+) {
+    fun createComment(createComments: CreateComments): UUID {
+        return with(createComments) {
+            Comments.create(
+                writer = memberRepository.findOneByUUID(writerUUID!!),
+                post = postRepository.findOneByUUID(postUUID!!),
+                content!!
+            ).run { commentsRepository.save(this).uuid }
+        }
+    }
+
+    fun editContent(updateCommentsContent: UpdateCommentsContent) {
+        with(updateCommentsContent) {
+            commentsRepository.findOneByUUID(uuid!!, writerUUID!!)
+                .also { it.editContent(content!!) }
+        }
+    }
+
+    fun deleteComment(deleteComments: DeleteComments) {
+        with(deleteComments) {
+            commentsRepository.findOneByUUID(uuid!!, writerUUID!!)
+                .also { commentsRepository.delete(it) }
+        }
+    }
+}
