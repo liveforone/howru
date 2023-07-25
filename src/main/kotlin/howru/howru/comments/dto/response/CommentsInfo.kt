6dbd3aca1b9606626a3524758529c306323db713
liveforone <yc4852@gmail.com>
2023-07-25 16:04:18 +0900

    Add comments info dto.

---
+++ src/main/kotlin/howru/howru/comments/dto/response/CommentsInfo.kt
@@ -0,0 +1,6 @@
+package howru.howru.comments.dto.response
+
+import howru.howru.comments.domain.CommentsState
+import java.util.UUID
+
+data class CommentsInfo(val uuid: UUID, val writerUUID: UUID, val postUUID: UUID, val content: String, val commentsState: CommentsState, val createdDate: Long)
