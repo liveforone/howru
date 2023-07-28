14d28fb609824e2aa509b0c0d3576c4b71791fdc
liveforone <yc4852@gmail.com>
2023-07-28 12:50:18 +0900

    Add reply url.

---
+++ src/main/kotlin/howru/howru/reply/controller/constant/ReplyUrl.kt
@@ -0,0 +1,10 @@
+package howru.howru.reply.controller.constant
+
+object ReplyUrl {
+    const val DETAIL = "/reply/detail/{uuid}"
+    const val BELONG_WRITER = "/reply/belong/writer/{writerUUID}"
+    const val BELONG_COMMENT = "/reply/belong/comment/{commentUUID}"
+    const val CREATE = "/reply/create"
+    const val EDIT = "/reply/edit"
+    const val DELETE = "/reply/delete"
+}
