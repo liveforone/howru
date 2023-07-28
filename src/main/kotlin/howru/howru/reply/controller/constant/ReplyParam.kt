891f9cd07fc0570986840c1934a53513540cb465
liveforone <yc4852@gmail.com>
2023-07-28 12:50:12 +0900

    Add reply param.

---
+++ src/main/kotlin/howru/howru/reply/controller/constant/ReplyParam.kt
@@ -0,0 +1,8 @@
+package howru.howru.reply.controller.constant
+
+object ReplyParam {
+    const val UUID = "uuid"
+    const val LAST_UUID = "lastUUID"
+    const val WRITER_UUID = "writerUUID"
+    const val COMMENT_UUID = "commentUUID"
+}
