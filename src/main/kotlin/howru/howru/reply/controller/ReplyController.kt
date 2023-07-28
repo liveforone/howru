83180ac4536786271ecffc846da9c37e3a0fea58
liveforone <yc4852@gmail.com>
2023-07-28 12:50:39 +0900

    Add reply controller.

---
+++ src/main/kotlin/howru/howru/reply/controller/ReplyController.kt
@@ -0,0 +1,95 @@
+package howru.howru.reply.controller
+
+import howru.howru.globalUtil.validateBinding
+import howru.howru.logger
+import howru.howru.reply.controller.constant.ReplyControllerLog
+import howru.howru.reply.controller.constant.ReplyParam
+import howru.howru.reply.controller.constant.ReplyUrl
+import howru.howru.reply.controller.response.ReplyResponse
+import howru.howru.reply.dto.request.CreateReply
+import howru.howru.reply.dto.request.DeleteReply
+import howru.howru.reply.dto.update.UpdateReplyContent
+import howru.howru.reply.service.command.ReplyCommandService
+import howru.howru.reply.service.query.ReplyQueryService
+import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+class ReplyController @Autowired constructor(
+    private val replyQueryService: ReplyQueryService,
+    private val replyCommandService: ReplyCommandService
+) {
+    @GetMapping(ReplyUrl.DETAIL)
+    fun detail(@PathVariable(ReplyParam.UUID) uuid: UUID): ResponseEntity<*> {
+        val reply = replyQueryService.getReplyByUUID(uuid)
+        return ReplyResponse.detailSuccess(reply)
+    }
+
+    @GetMapping(ReplyUrl.BELONG_WRITER)
+    fun belongWriter(
+        @PathVariable(ReplyParam.WRITER_UUID) writerUUID: UUID,
+        @RequestParam(ReplyParam.LAST_UUID, required = false) lastUUID: UUID?
+    ): ResponseEntity<*> {
+        val replies = replyQueryService.getRepliesByWriter(writerUUID, lastUUID)
+        return ReplyResponse.belongWriterSuccess(replies)
+    }
+
+    @GetMapping(ReplyUrl.BELONG_COMMENT)
+    fun belongComment(
+        @PathVariable(ReplyParam.COMMENT_UUID) commentUUID: UUID,
+        @RequestParam(ReplyParam.LAST_UUID, required = false) lastUUID: UUID?
+    ): ResponseEntity<*> {
+        val replies = replyQueryService.getRepliesByComment(commentUUID, lastUUID)
+        return ReplyResponse.belongCommentSuccess(replies)
+    }
+
+    @PostMapping(ReplyUrl.CREATE)
+    fun create(
+        @RequestBody @Valid createReply: CreateReply,
+        bindingResult: BindingResult
+    ): ResponseEntity<*> {
+        validateBinding(bindingResult)
+
+        replyCommandService.createReply(createReply)
+        logger().info(ReplyControllerLog.CREATE_SUCCESS.log)
+
+        return ReplyResponse.createReplySuccess()
+    }
+
+    @PutMapping(ReplyUrl.EDIT)
+    fun edit(
+        @RequestBody @Valid updateReplyContent: UpdateReplyContent,
+        bindingResult: BindingResult
+    ): ResponseEntity<*> {
+        validateBinding(bindingResult)
+
+        replyCommandService.editReply(updateReplyContent)
+        logger().info(ReplyControllerLog.EDIT_SUCCESS.log)
+
+        return ReplyResponse.editReplySuccess()
+    }
+
+    @DeleteMapping(ReplyUrl.DELETE)
+    fun delete(
+        @RequestBody @Valid deleteReply: DeleteReply,
+        bindingResult: BindingResult
+    ): ResponseEntity<*> {
+        validateBinding(bindingResult)
+
+        replyCommandService.deleteReply(deleteReply)
+        logger().info(ReplyControllerLog.DELETE_SUCCESS.log)
+
+        return ReplyResponse.deleteReplySuccess()
+    }
+}
