3f9343874fde01c374d7b447a1d3f74672c80cb0
liveforone <yc4852@gmail.com>
2023-07-28 12:47:31 +0900

    Add delete reply dto.

---
+++ src/main/kotlin/howru/howru/reply/dto/request/DeleteReply.kt
@@ -0,0 +1,11 @@
+package howru.howru.reply.dto.request
+
+import jakarta.validation.constraints.NotNull
+import java.util.*
+
+data class DeleteReply(
+    @field:NotNull(message = "대댓글의 외부식별자를 입력하세요.")
+    val uuid: UUID?,
+    @field:NotNull(message = "작성자의 외부식별자를 입력하세요.")
+    val writerUUID: UUID?
+)
