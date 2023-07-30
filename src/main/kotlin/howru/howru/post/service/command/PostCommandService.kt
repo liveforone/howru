d026ff7d083e81e40691b379d1a0f11e04f562e8
liveforone <yc4852@gmail.com>
2023-07-30 22:03:36 +0900

    Add evict post cache in command method.

--- src/main/kotlin/howru/howru/post/service/command/PostCommandService.kt
+++ src/main/kotlin/howru/howru/post/service/command/PostCommandService.kt
@@ -1,12 +1,15 @@
 package howru.howru.post.service.command
 
+import howru.howru.globalConfig.cache.constant.CacheName
 import howru.howru.member.repository.MemberRepository
+import howru.howru.post.cache.PostCache
 import howru.howru.post.domain.Post
 import howru.howru.post.dto.request.CreatePost
 import howru.howru.post.dto.request.DeletePost
 import howru.howru.post.dto.update.UpdatePostContent
 import howru.howru.post.repository.PostRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
@@ -17,6 +20,7 @@ class PostCommandService @Autowired constructor(
     private val postRepository: PostRepository,
     private val memberRepository: MemberRepository
 ) {
+    @CacheEvict(cacheNames = [CacheName.POST], key = PostCache.CREATE_WRITER)
     fun createPost(createPost: CreatePost): UUID {
         return with(createPost) {
             Post.create(writer = memberRepository.findOneByUUID(writerUUID!!), content!!)
@@ -24,6 +28,7 @@ class PostCommandService @Autowired constructor(
         }
     }
 
+    @CacheEvict(cacheNames = [CacheName.POST], key = PostCache.UPDATE_UUID)
     fun editContent(updatePostContent: UpdatePostContent) {
         with(updatePostContent) {
             postRepository.findOneByUUIDAndWriter(uuid!!, writerUUID!!)
@@ -31,6 +36,7 @@ class PostCommandService @Autowired constructor(
         }
     }
 
+    @CacheEvict(cacheNames = [CacheName.POST], key = PostCache.DELETE_UUID)
     fun deletePost(deletePost: DeletePost) {
         with(deletePost) {
             postRepository.findOneByUUIDAndWriter(uuid!!, writerUUID!!)
