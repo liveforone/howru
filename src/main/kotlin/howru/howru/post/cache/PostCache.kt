23dad2ca90fe2eb346a1badc4432bc6acecf799c
liveforone <yc4852@gmail.com>
2023-07-30 22:03:56 +0900

    Add post cache.

---
+++ src/main/kotlin/howru/howru/post/cache/PostCache.kt
@@ -0,0 +1,9 @@
+package howru.howru.post.cache
+
+object PostCache {
+    const val UUID_KEY = "#uuid"
+    const val WRITER_KEY = "#writerUUID"
+    const val CREATE_WRITER = "#createPost.writerUUID"
+    const val UPDATE_UUID = "#updatePostContent.uuid"
+    const val DELETE_UUID = "#deletePost.uuid"
+}
