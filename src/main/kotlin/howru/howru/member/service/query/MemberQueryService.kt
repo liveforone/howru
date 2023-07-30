48ac3c8e0e17d767b570fa0d21aef5659829881a
liveforone <yc4852@gmail.com>
2023-07-30 22:02:43 +0900

    Add find member data caching.

--- src/main/kotlin/howru/howru/member/service/query/MemberQueryService.kt
+++ src/main/kotlin/howru/howru/member/service/query/MemberQueryService.kt
@@ -1,7 +1,10 @@
 package howru.howru.member.service.query
 
+import howru.howru.globalConfig.cache.constant.CacheName
 import howru.howru.member.repository.MemberRepository
+import howru.howru.member.cache.MemberCache
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -11,5 +14,6 @@ import java.util.*
 class MemberQueryService @Autowired constructor(
     private val memberRepository: MemberRepository
 ) {
+    @Cacheable(cacheNames = [CacheName.MEMBER], key = MemberCache.KEY)
     fun getMemberByUUID(uuid: UUID) = memberRepository.findOneDtoByUUID(uuid)
 }
