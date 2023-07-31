2447698c55532c041c6e4a2e91a4b1977b7a5f78
liveforone <yc4852@gmail.com>
2023-07-31 19:10:10 +0900

    Add advertisement constant.

---
+++ src/main/kotlin/howru/howru/advertisement/domain/constant/AdvertisementConstant.kt
@@ -0,0 +1,7 @@
+package howru.howru.advertisement.domain.constant
+
+object AdvertisementConstant {
+    const val CONTENT_TYPE = "VARCHAR(800)"
+    const val HALF_MONTH: Long = 6
+    const val ONE_YEAR: Long = 1
+}
