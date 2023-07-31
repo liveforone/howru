efbc749ed5ba7eeab9516ca52e405f1d5bca7594
liveforone <yc4852@gmail.com>
2023-07-31 19:13:45 +0900

    Add advertisement controller advice.

---
+++ src/main/kotlin/howru/howru/exception/controllerAdvice/AdvertisementControllerAdvice.kt
@@ -0,0 +1,16 @@
+package howru.howru.exception.controllerAdvice
+
+import howru.howru.exception.exception.AdvertisementException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class AdvertisementControllerAdvice {
+    @ExceptionHandler(AdvertisementException::class)
+    fun advertisementException(advertisementException: AdvertisementException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(advertisementException.advertisementExceptionMessage.status)
+            .body(advertisementException.message)
+    }
+}
