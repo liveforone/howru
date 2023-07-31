0a1f9282a7c4dc44da995b79ec2a0682bed9a2ea
liveforone <yc4852@gmail.com>
2023-07-31 19:11:12 +0900

    Add cache name.

---
+++ src/main/kotlin/howru/howru/globalConfig/cache/constant/CacheName.kt
@@ -0,0 +1,7 @@
+package howru.howru.globalConfig.cache.constant
+
+object CacheName {
+    const val MEMBER = "memberQueryCache"
+    const val POST = "postQueryCache"
+    const val ADVERTISEMENT = "advertisementQueryCache"
+}
