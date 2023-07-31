ea8700aa83bea96f1f72aa94964544a5f4066299
liveforone <yc4852@gmail.com>
2023-07-31 19:10:00 +0900

    Add advertisement entity.

---
+++ src/main/kotlin/howru/howru/advertisement/domain/Advertisement.kt
@@ -0,0 +1,46 @@
+package howru.howru.advertisement.domain
+
+import howru.howru.advertisement.domain.constant.AdvertisementConstant
+import howru.howru.globalUtil.*
+import jakarta.persistence.*
+import java.time.LocalDate
+import java.util.*
+
+@Entity
+class Advertisement private constructor(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(columnDefinition = UUID_TYPE, unique = true, nullable = false) val uuid: UUID = createUUID(),
+    @Column(nullable = false) val company: String,
+    @Column(nullable = false) var title: String,
+    @Column(nullable = false, columnDefinition = AdvertisementConstant.CONTENT_TYPE) var content: String,
+    @Column(nullable = false, updatable = false, columnDefinition = DATE_TYPE) val createdDate: Int = getDateDigit(LocalDate.now()),
+    @Column(nullable = false, updatable = false, columnDefinition = DATE_TYPE) val endDate: Int
+) {
+    companion object {
+        fun createHalfAd(company: String, title: String, content: String): Advertisement {
+            return Advertisement(
+                company = company,
+                title = title,
+                content = content,
+                endDate = getDateDigit(LocalDate.now().plusMonths(AdvertisementConstant.HALF_MONTH))
+            )
+        }
+
+        fun createYearAd(company: String, title: String, content: String): Advertisement {
+            return Advertisement(
+                company = company,
+                title = title,
+                content = content,
+                endDate = getDateDigit(LocalDate.now().plusYears(AdvertisementConstant.ONE_YEAR))
+            )
+        }
+    }
+
+    fun editTitle(title: String) {
+        this.title = title
+    }
+
+    fun editContent(content: String) {
+        this.content = content
+    }
+}
