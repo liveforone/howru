811329e8abcfd54b5a75bcb8215e0f1b1c3abbe2
liveforone <yc4852@gmail.com>
2023-08-01 12:17:53 +0900

    Add advertisement controller log.

---
+++ src/main/kotlin/howru/howru/advertisement/controller/constant/AdvertisementControllerLog.kt
@@ -0,0 +1,8 @@
+package howru.howru.advertisement.controller.constant
+
+enum class AdvertisementControllerLog(val log: String) {
+    CREATE_SUCCESS("광고 생성성공"),
+    EDIT_TITLE_SUCCESS("광고 제목 수정 성공"),
+    EDIT_CONTENT_SUCCESS("광고 내용 수정 성공"),
+    DELETE_SUCCESS("광고 삭제 성공")
+}
