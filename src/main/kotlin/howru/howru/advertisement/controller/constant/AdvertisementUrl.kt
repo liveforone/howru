b50975890e66e43e93f0dce59e1c3243f6c8fc6c
liveforone <yc4852@gmail.com>
2023-08-01 12:16:34 +0900

    Add advertisement url.

---
+++ src/main/kotlin/howru/howru/advertisement/controller/constant/AdvertisementUrl.kt
@@ -0,0 +1,14 @@
+package howru.howru.advertisement.controller.constant
+
+object AdvertisementUrl {
+    const val DETAIL = "/advertisement/detail/{uuid}"
+    const val ALL_AD = "/advertisement/all"
+    const val SEARCH_COMPANY = "/advertisement/search-company"
+    const val EXPIRED_AD = "/advertisement/expired"
+    const val RANDOM = "/advertisement/random"
+    const val CREATE_HALF = "/advertisement/create/half"
+    const val CREATE_YEAR = "/advertisement/create/year"
+    const val EDIT_TITLE = "/advertisement/edit/title"
+    const val EDIT_CONTENT = "/advertisement/edit/content"
+    const val DELETE = "/advertisement/delete/{uuid}"
+}
