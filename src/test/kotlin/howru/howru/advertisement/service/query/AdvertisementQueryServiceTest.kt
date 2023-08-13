402e4f8bce7295d257a3b09e616c050dc4245fbc
liveforone <yc4852@gmail.com>
2023-08-13 19:02:37 +0900

    Remove un using import code.

--- src/test/kotlin/howru/howru/advertisement/service/query/AdvertisementQueryServiceTest.kt
+++ src/test/kotlin/howru/howru/advertisement/service/query/AdvertisementQueryServiceTest.kt
@@ -7,8 +7,6 @@ import howru.howru.member.service.command.MemberCommandService
 import jakarta.persistence.EntityManager
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
