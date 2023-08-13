e45b5783a5789e5c216afe2c87fdd2f91fb43e8e
liveforone <yc4852@gmail.com>
2023-08-13 18:49:45 +0900

    Add member controller constant.

---
+++ src/main/kotlin/howru/howru/member/controller/constant/MemberControllerConstant.kt
@@ -0,0 +1,7 @@
+package howru.howru.member.controller.constant
+
+object MemberControllerConstant {
+    const val ACCESS_TOKEN = "access-token"
+    const val REFRESH_TOKEN = "refresh-token"
+    const val MEMBER_UUID = "member-uuid"
+}
