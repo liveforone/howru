2115f8a151d5ef957ba6a0bcac09f4f3f9f23fba
liveforone <yc4852@gmail.com>
2023-08-13 18:49:12 +0900

    Remove token info.

--- src/main/kotlin/howru/howru/globalConfig/jwt/constant/JwtConstant.kt
+++ src/main/kotlin/howru/howru/globalConfig/jwt/constant/JwtConstant.kt
@@ -1,8 +1,6 @@
 package howru.howru.globalConfig.jwt.constant
 
 object JwtConstant {
-    const val ACCESS_TOKEN = "access-token"
-    const val REFRESH_TOKEN = "refresh-token"
     const val HEADER = "Authorization"
     const val CLAIM_NAME = "auth"
     const val TWO_HOUR_MS = 7200000
