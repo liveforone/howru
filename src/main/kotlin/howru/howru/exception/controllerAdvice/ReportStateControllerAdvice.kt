83d74e3d8fe453261f8c332c4d2be214738c303a
liveforone <yc4852@gmail.com>
2023-08-14 13:32:44 +0900

    Add report controller advice.

---
+++ src/main/kotlin/howru/howru/exception/controllerAdvice/ReportStateControllerAdvice.kt
@@ -0,0 +1,16 @@
+package howru.howru.exception.controllerAdvice
+
+import howru.howru.exception.exception.ReportStateException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ReportStateControllerAdvice {
+    @ExceptionHandler(ReportStateException::class)
+    fun reportStateExceptionHandle(reportStateException: ReportStateException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(reportStateException.repostStateExceptionMessage.status)
+            .body(reportStateException.message)
+    }
+}
