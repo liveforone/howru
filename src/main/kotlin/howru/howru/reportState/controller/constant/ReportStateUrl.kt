e18e7b100c9cf606884829882022f79bb5bff7c4
liveforone <yc4852@gmail.com>
2023-08-14 13:36:39 +0900

    Add report state url.

---
+++ src/main/kotlin/howru/howru/reportState/controller/constant/ReportStateUrl.kt
@@ -0,0 +1,6 @@
+package howru.howru.reportState.controller.constant
+
+object ReportStateUrl {
+    const val REPORT = "/report/member"
+    const val REPORT_STATE_INFO = "/report/info/{memberUUID}"
+}
