f514e1a7f43b88e202dfe196e6fbfb66fda59764
liveforone <yc4852@gmail.com>
2023-08-14 13:25:42 +0900

    Add member state and converter

---
+++ src/main/kotlin/howru/howru/converter/MemberStateConverter.kt
@@ -0,0 +1,11 @@
+package howru.howru.converter
+
+import howru.howru.reportState.domain.MemberState
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+
+@Converter
+class MemberStateConverter : AttributeConverter<MemberState, String> {
+    override fun convertToDatabaseColumn(attribute: MemberState) = attribute.name
+    override fun convertToEntityAttribute(dbData: String) = MemberState.valueOf(dbData)
+}
---
+++ src/main/kotlin/howru/howru/reportState/domain/MemberState.kt
@@ -0,0 +1,3 @@
+package howru.howru.reportState.domain
+
+enum class MemberState { NORMAL, SUSPEND_MONTH, SUSPEND_SIX_MONTH, SUSPEND_FOREVER }
