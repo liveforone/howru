5a4b26936aa94de8ca6b0a66dca1bb425bf396c8
liveforone <yc4852@gmail.com>
2023-08-14 13:25:59 +0900

    Add report state entity test.

---
+++ src/test/kotlin/howru/howru/reportState/domain/ReportStateTest.kt
@@ -0,0 +1,54 @@
+package howru.howru.reportState.domain
+
+import howru.howru.member.domain.Member
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ReportStateTest {
+
+    @Test
+    fun releaseSuspendTest() {
+        //given
+        val email = "test_member@gmail.com"
+        val pw = "1234"
+        val nickName = "test_nickName"
+        val member = Member.create(email, pw, nickName)
+        val reportState = ReportState.create(member)
+
+        //when
+        repeat(3) { reportState.addReport() }
+        reportState.releaseSuspend()
+
+        //then
+        Assertions.assertThat(reportState.memberState).isEqualTo(MemberState.SUSPEND_MONTH)
+    }
+
+    @Test
+    fun isSuspendTest() {
+        //given
+        val email = "test_member@gmail.com"
+        val pw = "1234"
+        val nickName = "test_nickName"
+        val member = Member.create(email, pw, nickName)
+        val reportState = ReportState.create(member)
+
+        //then
+        Assertions.assertThat(reportState.isNotSuspend()).isTrue()
+    }
+
+    @Test
+    fun addReportTest() {
+        //given
+        val email = "test_member@gmail.com"
+        val pw = "1234"
+        val nickName = "test_nickName"
+        val member = Member.create(email, pw, nickName)
+        val reportState = ReportState.create(member)
+
+        //when
+        repeat(3) { reportState.addReport() }
+
+        //then
+        Assertions.assertThat(reportState.isNotSuspend()).isFalse()
+    }
+}
