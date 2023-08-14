7adf6c645dc29313cd31eba7add1bda95dff8506
liveforone <yc4852@gmail.com>
2023-08-14 13:34:46 +0900

    Add report constant

--- src/main/kotlin/howru/howru/reportState/service/command/constant/ReportConstant.kt
+++ src/main/kotlin/howru/howru/reportState/service/command/constant/ReportConstant.kt
@@ -1,4 +1,4 @@
-package howru.howru.report.handler.constant
+package howru.howru.reportState.service.command.constant
 
 object ReportConstant {
     const val REPORT_MEMBER = "회원이 신고되었습니다. UUID : "
