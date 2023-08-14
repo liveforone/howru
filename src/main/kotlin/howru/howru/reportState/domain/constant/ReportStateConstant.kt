ef2650fb3db34b72dd29237d3600eac5053359ce
liveforone <yc4852@gmail.com>
2023-08-14 13:26:08 +0900

    Add report state constant.

---
+++ src/main/kotlin/howru/howru/reportState/domain/constant/ReportStateConstant.kt
@@ -0,0 +1,12 @@
+package howru.howru.reportState.domain.constant
+
+object ReportStateConstant {
+    const val MEMBER_STATE_TYPE = "VARCHAR(17)"
+    const val BASIC_REPORT = 0
+    const val ONE_MONTH: Long = 1
+    const val SIX_MONTH: Long = 6
+    const val BASIC_VARIATION = 1
+    const val SUSPEND_MONTH_CNT = 3
+    const val SUSPEND_SIX_MONTH_CNT = 6
+    const val SUSPEND_FOREVER_CNT = 9
+}
