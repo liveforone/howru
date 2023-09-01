3de8b6d7d488ffc440b88a0b67d747ba80d93732
liveforone <yc4852@gmail.com>
2023-09-01 17:44:29 +0900

    Refactor code structure.

--- src/main/kotlin/howru/howru/globalUtil/PasswordUtil.kt
+++ src/main/kotlin/howru/howru/globalUtil/PasswordUtil.kt
@@ -2,5 +2,6 @@ package howru.howru.globalUtil
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
-fun encodePassword(password:String): String = BCryptPasswordEncoder().encode(password)
-fun isMatchPassword(password: String, originalPassword:String) = BCryptPasswordEncoder().matches(password, originalPassword)
+fun encodePassword(password: String): String = BCryptPasswordEncoder().encode(password)
+fun isMatchPassword(password: String, originalPassword: String) =
+    BCryptPasswordEncoder().matches(password, originalPassword)
