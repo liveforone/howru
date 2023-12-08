9f15499accb51395d1195092db4a487b1b7ddc80
liveforone <yc4852@gmail.com>
2023-12-08 21:48:48 +0900

    Add refresh token expired date

--- src/main/kotlin/howru/howru/globalConfig/jwt/constant/JwtConstant.kt
+++ src/main/kotlin/howru/howru/globalConfig/jwt/constant/JwtConstant.kt
@@ -7,6 +7,7 @@ object JwtConstant {
     const val EMPTY_PW = ""
     const val CREDENTIAL = ""
     const val TWO_HOUR_MS = 7200000
+    const val THIRTY_DAY = 2592000000
     const val BEARER_TOKEN = "Bearer"
     const val SECRET_KEY_PATH = "\${jwt.secret}"
     const val TOKEN_SUB_INDEX = 7
