9d49521cd1026b6ffe10c1f46c282ed147a04a32
liveforone <yc4852@gmail.com>
2023-12-12 22:24:43 +0900

    Add token reissue and logout controller log

--- src/main/kotlin/howru/howru/member/controller/constant/MemberControllerLog.kt
+++ src/main/kotlin/howru/howru/member/controller/constant/MemberControllerLog.kt
@@ -7,5 +7,7 @@ enum class MemberControllerLog(val log: String) {
     UPDATE_PW_SUCCESS("비밀번호 변경 성공"),
     LOCK_ON_SUCCESS("회원 잠금 성공"),
     LOCK_OFF_SUCCESS("회원 잠금 해제 성공"),
-    WITHDRAW_SUCCESS("회원탈퇴 성공")
+    LOGOUT_SUCCESS("회원 로그아웃 성공"),
+    WITHDRAW_SUCCESS("회원탈퇴 성공"),
+    JWT_TOKEN_REISSUE("JWT 토큰 갱신 성공")
 }
