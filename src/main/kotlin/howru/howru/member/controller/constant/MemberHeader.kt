762deb7055f44b653b8727b8e24b7ed958c48bad
liveforone <yc4852@gmail.com>
2023-12-12 22:20:07 +0900

    Add member header constant

---
+++ src/main/kotlin/howru/howru/member/controller/constant/MemberHeader.kt
@@ -0,0 +1,6 @@
+package howru.howru.member.controller.constant
+
+object MemberHeader {
+    const val UUID = "uuid"
+    const val REFRESH_TOKEN = "refresh-token"
+}
--- src/main/kotlin/howru/howru/member/controller/constant/MemberParam.kt
+++
@@ -1,3 +0,0 @@
-package howru.howru.member.controller.constant
-
-object MemberParam { const val UUID = "uuid" }
