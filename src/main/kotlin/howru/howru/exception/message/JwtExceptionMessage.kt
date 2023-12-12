5daecc9218a4bedb3b79963922e7840ba6dbd78a
liveforone <yc4852@gmail.com>
2023-12-12 22:27:10 +0900

    Add not exist refresh token and un match refresh token exception message

--- src/main/kotlin/howru/howru/exception/message/JwtExceptionMessage.kt
+++ src/main/kotlin/howru/howru/exception/message/JwtExceptionMessage.kt
@@ -2,9 +2,10 @@ package howru.howru.exception.message
 
 enum class JwtExceptionMessage(val status: Int, val message: String) {
     TOKEN_IS_NULL(404, "Token Is Null"),
-    INVALID_MESSAGE(401, "Invalid JWT Token"),
-    EXPIRED_MESSAGE(401, "Expired JWT Token"),
-    UNSUPPORTED_MESSAGE(401, "Unsupported JWT Token"),
+    INVALID_TOKEN(401, "Invalid JWT Token"),
+    EXPIRED_JWT_TOKEN(401, "Expired Jwt Token"),
+    UNSUPPORTED(401, "Unsupported JWT Token"),
     EMPTY_CLAIMS(404, "JWT claims string is empty."),
-    NOT_EXIST_REFRESH_TOKEN(404, "Refresh Token is not exist")
+    NOT_EXIST_REFRESH_TOKEN(404, "Refresh Token is not exist"),
+    UN_MATCH_REFRESH_TOKEN(401, "Un Match Refresh Token")
 }
