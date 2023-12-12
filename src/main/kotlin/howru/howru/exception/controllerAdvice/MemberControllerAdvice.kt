4bd0c41f257b59581cc0b01a77d334eeb4ae7d30
liveforone <yc4852@gmail.com>
2023-12-12 22:25:06 +0900

    Add jwt custom exception handler method in controller advice

--- src/main/kotlin/howru/howru/exception/controllerAdvice/MemberControllerAdvice.kt
+++ src/main/kotlin/howru/howru/exception/controllerAdvice/MemberControllerAdvice.kt
@@ -1,6 +1,7 @@
 package howru.howru.exception.controllerAdvice
 
 import howru.howru.exception.controllerAdvice.constant.MemberAdviceConstant
+import howru.howru.exception.exception.JwtCustomException
 import howru.howru.exception.exception.MemberException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -23,5 +24,12 @@ class MemberControllerAdvice {
         return ResponseEntity
             .status(memberException.memberExceptionMessage.status)
             .body(memberException.message + memberException.memberIdentifier)
+    }
+
+    @ExceptionHandler(JwtCustomException::class)
+    fun jwtCustomExceptionHandle(jwtCustomException: JwtCustomException): ResponseEntity<String> {
+        return ResponseEntity
+            .status(jwtCustomException.jwtExceptionMessage.status)
+            .body(jwtCustomException.message)
     }
 }
