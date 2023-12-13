461532ceb97182e44ba475129c1a1fc9f8a608d8
liveforone <yc4852@gmail.com>
2023-12-13 10:39:05 +0900

    Refactor comment controller log type, before enum class, now object class

--- src/main/kotlin/howru/howru/comments/controller/constant/CommentsControllerLog.kt
+++
@@ -1,7 +0,0 @@
-package howru.howru.comments.controller.constant
-
-enum class CommentsControllerLog(val log: String) {
-    CREATE_COMMENT_SUCCESS("댓글 등록 성공"),
-    EDIT_COMMENT_SUCCESS("댓글 수정 성공"),
-    DELETE_COMMENTS_SUCCESS("댓글 삭제 성공")
-}
---
+++ src/main/kotlin/howru/howru/comments/log/CommentsControllerLog.kt
@@ -0,0 +1,7 @@
+package howru.howru.comments.log
+
+object CommentsControllerLog {
+    const val CREATE_COMMENT_SUCCESS = "댓글 등록 성공, 회원 UUID : "
+    const val EDIT_COMMENT_SUCCESS = "댓글 수정 성공, 댓글 ID : "
+    const val DELETE_COMMENTS_SUCCESS = "댓글 삭제 성공, 댓글 ID : "
+}
