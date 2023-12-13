1dfb96e19441999a4b2a289bdf82c65cc5dab9f6
liveforone <yc4852@gmail.com>
2023-12-13 10:33:28 +0900

    Refactor log identifier in ad controller log

--- src/main/kotlin/howru/howru/advertisement/log/AdControllerLog.kt
+++ src/main/kotlin/howru/howru/advertisement/log/AdControllerLog.kt
@@ -2,7 +2,7 @@ package howru.howru.advertisement.log
 
 object AdControllerLog {
     const val CREATE_SUCCESS = "광고 생성성공, 광고주 : "
-    const val EDIT_TITLE_SUCCESS = "광고 제목 수정 성공, ID : "
-    const val EDIT_CONTENT_SUCCESS = "광고 내용 수정 성공, ID : "
-    const val DELETE_SUCCESS = "광고 삭제 성공, ID : "
+    const val EDIT_TITLE_SUCCESS = "광고 제목 수정 성공, 광고 ID : "
+    const val EDIT_CONTENT_SUCCESS = "광고 내용 수정 성공, 광고 ID : "
+    const val DELETE_SUCCESS = "광고 삭제 성공, 광고 ID : "
 }
