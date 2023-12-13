5949717ba62bc7353279813e63e7753019031e62
liveforone <yc4852@gmail.com>
2023-12-13 10:27:04 +0900

    Refactor advertisement controller log type. before enum, now object class

---
+++ src/main/kotlin/howru/howru/advertisement/log/AdControllerLog.kt
@@ -0,0 +1,8 @@
+package howru.howru.advertisement.log
+
+object AdControllerLog {
+    const val CREATE_SUCCESS = "광고 생성성공, 광고주 : "
+    const val EDIT_TITLE_SUCCESS = "광고 제목 수정 성공, ID : "
+    const val EDIT_CONTENT_SUCCESS = "광고 내용 수정 성공, ID : "
+    const val DELETE_SUCCESS = "광고 삭제 성공, ID : "
+}
