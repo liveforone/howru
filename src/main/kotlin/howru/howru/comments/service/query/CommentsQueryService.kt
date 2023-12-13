f90ff83e21871662e05dcefde81891382a260a8b
liveforone <yc4852@gmail.com>
2023-12-13 10:39:59 +0900

    Add comments service log and insert in query service

--- src/main/kotlin/howru/howru/comments/service/query/CommentsQueryService.kt
+++ src/main/kotlin/howru/howru/comments/service/query/CommentsQueryService.kt
@@ -1,9 +1,11 @@
 package howru.howru.comments.service.query
 
 import howru.howru.comments.dto.response.CommentsInfo
+import howru.howru.comments.log.CommentsServiceLog
 import howru.howru.comments.repository.CommentsRepository
 import howru.howru.exception.exception.SubscribeException
 import howru.howru.exception.message.SubscribeExceptionMessage
+import howru.howru.logger
 import howru.howru.subscribe.service.query.SubscribeQueryService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -20,7 +22,10 @@ class CommentsQueryService @Autowired constructor(
     fun getCommentsByWriter(writerUUID: UUID, lastId: Long?) = commentsRepository.findCommentsByWriter(writerUUID, lastId)
     fun getCommentsByPost(postId: Long, lastId: Long?) = commentsRepository.findCommentsByPost(postId, lastId)
     fun getCommentsBySomeone(someoneUUID: UUID, memberUUID: UUID, lastId: Long?): List<CommentsInfo> {
-        require(subscribeQueryService.isFollowEach(someoneUUID, memberUUID)) { throw SubscribeException(SubscribeExceptionMessage.IS_NOT_FOLLOW_EACH, memberUUID) }
+        require(subscribeQueryService.isFollowEach(someoneUUID, memberUUID)) {
+            logger().info(CommentsServiceLog.VIEW_SOMEONE_COMMENTS_WHO_NOT_FOLLOWING + memberUUID)
+            throw SubscribeException(SubscribeExceptionMessage.IS_NOT_FOLLOW_EACH, memberUUID)
+        }
         return commentsRepository.findCommentsByWriter(someoneUUID, lastId)
     }
 }
