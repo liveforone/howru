622e1bedbe60456fceb86639dcbc1a7ef8492ef5
liveforone <yc4852@gmail.com>
2023-12-19 20:09:49 +0900

    Refactor auth size changed

--- src/main/kotlin/howru/howru/member/domain/constant/MemberConstant.kt
+++ src/main/kotlin/howru/howru/member/domain/constant/MemberConstant.kt
@@ -2,7 +2,7 @@ package howru.howru.member.domain.constant
 
 object MemberConstant {
     const val PW_TYPE = "VARCHAR(100)"
-    const val ROLE_TYPE = "VARCHAR(7)"
+    const val ROLE_TYPE = "VARCHAR(13)"
     const val LOCK_TYPE = "VARCHAR(3)"
     const val NICKNAME_TYPE = "VARCHAR(10)"
     const val ADMIN_EMAIL = "admin_howru@gmail.com"
