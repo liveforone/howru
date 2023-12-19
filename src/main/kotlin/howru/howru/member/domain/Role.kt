a77534a7f21df32d9975f155f0b6231325bcc306
liveforone <yc4852@gmail.com>
2023-12-19 20:10:20 +0900

    Add withdraw auth

--- src/main/kotlin/howru/howru/member/domain/Role.kt
+++ src/main/kotlin/howru/howru/member/domain/Role.kt
@@ -1,3 +1,3 @@
 package howru.howru.member.domain
 
-enum class Role(val auth:String) { MEMBER("ROLE_MEMBER"), ADMIN("ROLE_ADMIN") }
+enum class Role(val auth:String) { MEMBER("ROLE_MEMBER"), ADMIN("ROLE_ADMIN"), WITHDRAW("ROLE_WITHDRAW") }
