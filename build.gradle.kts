3e5789c26502595d3731fd4a02460cf144f6c02b
liveforone <yc4852@gmail.com>
2024-02-19 16:42:24 +0900

    Upgrade java and gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ group = "howru"
 version = "2.1.0"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -64,7 +64,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+		jvmTarget = "21"
 	}
 }
 
