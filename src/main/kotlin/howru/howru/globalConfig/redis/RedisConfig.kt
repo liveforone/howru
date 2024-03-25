943b2d5cb3aacdc48deb7acc7dbfbdc4ff4949a3
liveforone <yc4852@gmail.com>
2024-03-25 15:52:00 +0900

    Add redis config

---
+++ src/main/kotlin/howru/howru/globalConfig/redis/RedisConfig.kt
@@ -0,0 +1,79 @@
+package howru.howru.globalConfig.redis
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import howru.howru.globalConfig.redis.constant.RedisConstant
+import io.lettuce.core.RedisURI
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+@EnableCaching
+class RedisConfig(
+    @Value(RedisConstant.REDIS_URL)
+    val url: String
+) {
+    @Bean
+    fun lettuceConnectionFactory(): LettuceConnectionFactory {
+        val redisURI: RedisURI = RedisURI.create(url)
+        val configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI)
+        return LettuceConnectionFactory(configuration)
+    }
+
+    @Bean
+    fun cacheManager(): CacheManager {
+        val objectMapper = ObjectMapper()
+            .registerModules(
+                JavaTimeModule(),
+                KotlinModule.Builder()
+                    .withReflectionCacheSize(RedisConstant.REFLECTION_CACHE_SIZE)
+                    .configure(KotlinFeature.NullToEmptyCollection, false)
+                    .configure(KotlinFeature.NullToEmptyMap, false)
+                    .configure(KotlinFeature.NullIsSameAsDefault, false)
+                    .configure(KotlinFeature.SingletonSupport, false)
+                    .configure(KotlinFeature.StrictNullChecks, false)
+                    .build()
+            )
+            .activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder()
+                    .allowIfBaseType(Any::class.java).build(), ObjectMapper.DefaultTyping.EVERYTHING
+            )
+        val configuration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer(objectMapper)
+                ))
+            .entryTtl(Duration.ofMinutes(RedisConstant.TTL))
+            .disableCachingNullValues()
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(lettuceConnectionFactory())
+            .cacheDefaults(configuration)
+            .build()
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        return RedisTemplate<String, Any>().apply {
+            this.connectionFactory = lettuceConnectionFactory()
+            this.keySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+            this.hashKeySerializer = StringRedisSerializer()
+            this.hashValueSerializer = StringRedisSerializer()
+        }
+    }
+}
