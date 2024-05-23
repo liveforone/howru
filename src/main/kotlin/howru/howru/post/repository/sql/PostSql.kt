2bcd1726b1116bdf42378da7c1f2137ea3326360
liveforone <yc4852@gmail.com>
2024-05-23 18:16:06 +0900

    Refactor dto location in jpql query

--- src/main/kotlin/howru/howru/post/repository/sql/PostSql.kt
+++ src/main/kotlin/howru/howru/post/repository/sql/PostSql.kt
@@ -2,7 +2,7 @@ package howru.howru.post.repository.sql
 
 object PostSql {
     const val RANDOM_POSTS_QUERY = """
-    select new howru.howru.post.domain.vo.PostInfo(p.id, p.writer.id, p.content, p.postState, p.createdDatetime)
+    select new howru.howru.post.dto.response.PostInfo(p.id, p.writer.id, p.content, p.postState, p.createdDatetime)
      from Post p order by RAND() limit 10"""
     const val COUNT_OF_POST_BY_WRITER_QUERY = "select count(*) from Post p where p.writer.id = :writerId"
     const val WRITER_ID = "writerId"
