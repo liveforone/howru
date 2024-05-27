54f5b29ea3568264283e05ead41cc7456f74fdd5
liveforone <yc4852@gmail.com>
2024-05-27 22:36:31 +0900

    Remove aop cache logic remove. Refactor only use redis template

--- src/main/kotlin/howru/howru/global/config/redis/RedisConfig.kt
+++ src/main/kotlin/howru/howru/global/config/redis/RedisConfig.kt
@@ -1,25 +1,14 @@
 package howru.howru.global.config.redis
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinFeature
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import howru.howru.global.config.redis.constant.RedisConstant
 import io.lettuce.core.RedisURI
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.cache.RedisCacheConfiguration
-import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.StringRedisSerializer
-import java.time.Duration
+import org.springframework.data.redis.serializer.*
 
 @Configuration
 @EnableCaching
@@ -32,42 +21,6 @@ class RedisConfig(
         val redisURI: RedisURI = RedisURI.create(url)
         val configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI)
         return LettuceConnectionFactory(configuration)
-    }
-
-    @Bean
-    fun cacheManager(): CacheManager {
-        val objectMapper =
-            ObjectMapper()
-                .registerModules(
-                    JavaTimeModule(),
-                    KotlinModule.Builder()
-                        .withReflectionCacheSize(RedisConstant.REFLECTION_CACHE_SIZE)
-                        .configure(KotlinFeature.NullToEmptyCollection, false)
-                        .configure(KotlinFeature.NullToEmptyMap, false)
-                        .configure(KotlinFeature.NullIsSameAsDefault, false)
-                        .configure(KotlinFeature.SingletonSupport, false)
-                        .configure(KotlinFeature.StrictNullChecks, false)
-                        .build()
-                )
-                .activateDefaultTyping(
-                    BasicPolymorphicTypeValidator.builder()
-                        .allowIfBaseType(Any::class.java).build(),
-                    ObjectMapper.DefaultTyping.EVERYTHING
-                )
-        val configuration =
-            RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
-                .serializeValuesWith(
-                    RedisSerializationContext.SerializationPair.fromSerializer(
-                        GenericJackson2JsonRedisSerializer(objectMapper)
-                    )
-                )
-                .entryTtl(Duration.ofMinutes(RedisConstant.TTL))
-                .disableCachingNullValues()
-        return RedisCacheManager.RedisCacheManagerBuilder
-            .fromConnectionFactory(lettuceConnectionFactory())
-            .cacheDefaults(configuration)
-            .build()
     }
 
     @Bean
