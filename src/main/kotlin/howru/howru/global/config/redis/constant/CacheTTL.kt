f10c4fc4fcc34a80b6af0681cb8321876867112f
liveforone <yc4852@gmail.com>
2024-05-27 22:35:18 +0900

    Add cache ttl constant

---
+++ src/main/kotlin/howru/howru/global/config/redis/constant/CacheTTL.kt
@@ -0,0 +1,19 @@
+package howru.howru.global.config.redis.constant
+
+object CacheTTL {
+    const val ONE: Long = 1
+    const val TWO: Long = 2
+    const val THREE: Long = 3
+    const val FOUR: Long = 4
+    const val FIVE: Long = 5
+    const val SIX: Long = 6
+    const val SEVEN: Long = 7
+    const val EIGHT: Long = 8
+    const val NINE: Long = 9
+    const val TEN: Long = 10
+    const val ELEVEN: Long = 11
+    const val TWELVE: Long = 12
+    const val THIRTEEN: Long = 13
+    const val FOURTEEN: Long = 14
+    const val FIFTEEN: Long = 15
+}
