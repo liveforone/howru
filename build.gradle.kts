5ef1d8618c7dfae92212f043d3040abd604f29ea
liveforone <yc4852@gmail.com>
2024-09-28 17:25:14 +0900

    Upgrade spring boot version to 3.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    id("org.springframework.boot") version "3.3.2"
+    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.spring") version "2.0.10"
-    kotlin("plugin.jpa") version "2.0.10"
-    kotlin("kapt") version "2.0.10"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.spring") version "2.0.20"
+    kotlin("plugin.jpa") version "2.0.20"
+    kotlin("kapt") version "2.0.20"
 }
 
 allOpen {
