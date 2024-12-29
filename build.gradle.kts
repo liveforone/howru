58e14a1dfec2fd263e425e4d3c560caee4c7db61
liveforone <yc4852@gmail.com>
2024-12-29 17:25:04 +0900

    Add swagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     // jwt config
