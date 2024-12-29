51dc8865d0732079c389fb629c090e0d8b12b7ad
liveforone <yc4852@gmail.com>
2024-12-29 17:25:25 +0900

    Add swagger url in permit all api list

--- src/main/kotlin/howru/howru/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/howru/howru/global/config/security/SecurityConfig.kt
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +32,10 @@ class SecurityConfig
             http.sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             http.authorizeHttpRequests { path ->
                 path.requestMatchers(
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/api-docs/**",
+                    "/v3/api-docs/**",
                     MemberUrl.SIGNUP,
                     MemberUrl.LOGIN,
                     MemberUrl.JWT_TOKEN_REISSUE,
